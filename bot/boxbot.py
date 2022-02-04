a63f23506a81d2eb187c2233a61ed48354146c7c
Tempystral <elijah.doern@gmail.com>
2022-02-03 23:52:07 -0500

    Add listeners and aiohttp session to bot

--- bot/boxbot.py
+++ bot/boxbot.py
@@ -1,8 +1,7 @@
-from pathlib import Path
+import aiohttp
 import hikari
 import lightbulb
 import logging
-#from utils import boxconfig, logger
 
 logger = logging.getLogger("BoxBot")
 
@@ -17,17 +16,23 @@ class BoxBot(lightbulb.BotApp):
                          
                          banner="bot")
 
-    def on_starting(self) -> None:
+    async def on_starting(self, event:hikari.Event) -> None:
         logger.info("Starting...")
+        self.d.aio_session = aiohttp.ClientSession()
     
-    def on_started(self) -> None:
+    async def on_started(self, event:hikari.Event) -> None:
         logger.info(f"Logged in as:\n\t{self.user}\nwith ID:\n\t{self.user.id}")
         logger.info("Logged into guilds:\n\t{guilds}".format(guilds="\n\t".join((f"{s.name}:{s.id}" for s in self.guilds))))
 
-    def on_stopping(self) -> None:
+    async def on_stopping(self, event:hikari.Event) -> None:
         logger.info("Shutting down...")
+        await self.d.aio_session.close()
 
 def create(token:str, guild_id:str) -> BoxBot:
-    bot = BoxBot(token, guild_id)
+    bot = BoxBot(token, guild_id) # init
+    # Listen for system events
+    bot.subscribe(hikari.StartingEvent, bot.on_starting)
+    bot.subscribe(hikari.StoppingEvent, bot.on_stopping)
+    # Load extensions
     bot.load_extensions_from("./modules/", must_exist=False)
     return bot
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 import os
+import asyncio
 from bot import create
 from decouple import config
 
 if __name__ == "__main__":
 	if os.name != "nt":
 			import uvloop
-			uvloop.install()
-
+			asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 	create(
 		config("BOT_TOKEN"),
 		config("GUILD", cast=int)
