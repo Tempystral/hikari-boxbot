62695a0fbf3f7f6df6cc2cc0a7ba9ea2f8714e2a
Tempystral <elijah.doern@gmail.com>
2022-02-04 20:26:12 -0500

    Only get msg from Discord once during checks, add error type for cleanup
    Use the lightbulb datastore to keep a message in memory while it's being checked. This reduces the chance of being rate-limited. However if one of these fails, throw the message too so the error handler can remove it from the datastore.

--- sauce/checks.py
+++ sauce/checks.py
@@ -5,21 +5,19 @@ import logging
 logger = logging.getLogger("BoxBot.sauce.checks")
 
 async def _user_replied_to(context: lightbulb.Context) -> bool:
-  msg = await context.bot.rest.fetch_message(context.interaction.channel_id,
-                                             context.interaction.target_id)
+  msg:hikari.Message = await get_message_in_datastore(context)
   try:
     if not msg.referenced_message.author == context.author:
-      raise NotCallingAuthorError("Only the original poster or a mod can remove this post.")
+      raise NotCallingAuthorError("Only the original poster or a mod can remove this post.", data=msg)
   except AttributeError as e:
     pass # This should be ignored because it's optional AND getting checked again in reply_only
   return True
 
 async def _reply_only(context: lightbulb.Context) -> bool:
   if context.interaction.type == hikari.InteractionType.APPLICATION_COMMAND:
-    msg = await context.bot.rest.fetch_message(context.interaction.channel_id,
-                                               context.interaction.target_id)
+    msg:hikari.Message = await get_message_in_datastore(context)
     if msg.type is not hikari.MessageType.REPLY:
-      raise lightbulb.errors.CheckFailure("This command can only be used on replies.")
+      raise NotAReplyError("This command can only be used on replies.", data=msg)
     return True
 
 def _on_bot_message(context: lightbulb.Context) -> bool:
@@ -29,6 +27,18 @@ def _on_bot_message(context: lightbulb.Context) -> bool:
     if not msg.author.is_bot:
       raise BotMessageOnlyError("This command can only be run on messages sent by a bot.")
     return True
+
+async def get_message_in_datastore(context: lightbulb.Context) -> bool:
+  msg = context.bot.d.get(context.interaction.target_id)
+  return (msg
+          if msg
+          else context.bot.d.setdefault(context.interaction.target_id,
+                                        await _get_message(context))
+  ) # If we already have a message ID saved, don't get it again to avoid the rate limit
+
+async def _get_message(context: lightbulb.Context) -> bool:
+  logger.debug("Fetching message from Discord...")
+  return await context.bot.rest.fetch_message(context.interaction.channel_id,context.interaction.target_id)
 
 user_replied_to = lightbulb.Check(p_callback=_user_replied_to, m_callback=_user_replied_to)
 """
@@ -45,12 +55,20 @@ on_bot_message = lightbulb.Check(p_callback=_on_bot_message, m_callback=_on_bot_
 Only run command on messages sent by a bot.
 """
 
-class NotAReplyError(lightbulb.errors.CheckFailure):
+class CheckFailureWithData(lightbulb.errors.CheckFailure):
+  """
+  A wrapper class for errors containing cleanup data.
+  """
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args)
+    self.data = kwargs.get("data")
+
+class NotAReplyError(CheckFailureWithData):
   """
   The message selected by the calling check is not a reply.
   """
 
-class NotCallingAuthorError(lightbulb.errors.CheckFailure):
+class NotCallingAuthorError(CheckFailureWithData):
   """
   The calling user is not the user of the original sauced message.
   """
