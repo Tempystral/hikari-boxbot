adfe512f681b34e8189c7857f5d92ffe57c44b1a
Tempystral <elijah.doern@gmail.com>
2022-02-04 20:27:20 -0500

    Better logging, fix bug in find_links(), error handling + cleanup

--- modules/sauce.py
+++ modules/sauce.py
@@ -1,16 +1,17 @@
+import logging
 from re import Match
 from typing import Tuple
-from decouple import config
 
 import hikari
-import lightbulb as lb
 
-from sauce import SauceResponse, util
+import lightbulb as lb
+from decouple import config
+from sauce import SauceResponse, util, checks
 from sauce.checks import on_bot_message, reply_only, user_replied_to
+from sauce.checks import CheckFailureWithData
 from sauce.ladles.abc import Ladle
 
-import logging
-logger = logging.getLogger("Sauce")
+logger = logging.getLogger("BoxBot.modules.sauce")
 
 sauce_plugin = lb.Plugin("Sauce")
 
@@ -21,12 +22,14 @@ async def sauce(event: hikari.GuildMessageCreateEvent):
   
   # Get a list of links from the message
   msg = util.remove_spoilered_text(event.content)
+  logger.debug(f"Message: {msg}")
   links = find_links(msg)
+  if links: logger.debug(f"Found the following links: {[m for _, m in links]}")
 
   # If the regex finds a suitable match, send the link to one of the ladles to retrieve metadata
   for ladle, matched_link in links:
     response:SauceResponse = await ladle.extract(match=matched_link, session=sauce_plugin.bot.d.aio_session)
-
+    logger.debug(f"Extracted Data: {response}")
     # Once metadata is retrieved, send it off to the embed generator
     embed = response.to_embed()
     # Post the embed + suppress embeds on original message
@@ -35,17 +38,25 @@ async def sauce(event: hikari.GuildMessageCreateEvent):
       await event.message.respond(embed, reply=event.message.id, mentions_reply=False) # Reply, but don't mention. We can reference this value later.
 
 @sauce_plugin.command
-@lb.add_checks(on_bot_message, reply_only, user_replied_to | lb.has_roles(role1=sauce_plugin.bot.d.roles))
-@lb.command("oops", "Purge the last message BoxBot sauced for you.")
+@lb.add_checks(on_bot_message, reply_only, user_replied_to | lb.has_roles(role1=config("ELEVATED_ROLES", cast=str)))
+@lb.command("Un-Sauce", "Purge the last message BoxBot sauced for you.")
 @lb.implements(lb.MessageCommand)
 async def oops(ctx: lb.MessageContext) -> None:
-  message:hikari.Message = ctx.options.target
-  msg = await ctx.bot.rest.fetch_message(message.channel_id, message.id)
+  msg = ctx.bot.d.pop(ctx.interaction.target_id)
   await ctx.respond(f"Ran command {ctx.command.name} on {msg.id}\nMessage type: {msg.type}\nReferenced message: {msg.referenced_message.id}\n")
 
 @sauce_plugin.set_error_handler
 async def on_error(event: lb.events.CommandErrorEvent) -> None:
-  logger.warning(f"Exception triggered: {event.exception}")
+  logger.warning(f"Caught exception: {type(event.exception)}")
+
+  if isinstance(event.exception, CheckFailureWithData):
+    msg:hikari.Message = event.exception.data
+    try:
+      # Remove the stored message from the datastore so it doesn't grow
+      event.bot.d.pop(msg.id)
+      logger.debug(f"Cleaned message {msg.id} from the datastore")
+    except (AttributeError, KeyError) as e:
+      logger.debug(f"Message {msg.id} not found in datastore.")
   await event.context.respond("\n".join(event.exception.args[0].split(", ")), flags=hikari.MessageFlag.EPHEMERAL)
   return True # To tell the bot not to propogate this error event up the chain
 
@@ -54,15 +65,15 @@ def find_links(message:str) -> list[Tuple[Ladle, Match]]:
   for ext, pattern in sauce_plugin.bot.d.extractors:
     # Find all the matches for a given extractor's pattern in the message
     matches = pattern.finditer(message)
+    logger.debug(f"Matches for pattern: {pattern}: {matches}")
     if matches:
-      return ([(ext, match) for match in matches])
+      links.extend([(ext, match) for match in matches])
   return links
 
 def load(bot: lb.BotApp) -> None:
-
-  bot.d.roles = config("ELEVATED_ROLES", cast=str)
-  l_extractors = config("EXTRACTORS", cast=str, strip=" ").split(",")
+  l_extractors = config("EXTRACTORS", cast=str).split(",")
   bot.d.extractors = util.compile_patterns(util.get_ladles(l_extractors))
+  logger.debug(f"Loaded extractors: {[(l, e.pattern) for l, e in bot.d.extractors]}")
   bot.add_plugin(sauce_plugin)
 
 def unload(bot: lb.BotApp) -> None:
