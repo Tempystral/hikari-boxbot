9256cd9d1d7b12854d61b19eb9367422c08d5d9a
Tempystral <elijah.doern@gmail.com>
2022-02-13 22:34:37 -0500

    Fix indentation, add cleanup() to ladles

--- sauce/ladles/furaffinity.py
+++ sauce/ladles/furaffinity.py
@@ -8,31 +8,34 @@ from sauce import SauceResponse
 
 
 class Furaffinity(Ladle):
-    def __init__(self):
-        self.pattern = r'https?://www.furaffinity.net/(?:view|full)/(?P<id>\d+)'
-        self._cookie = config("FA_COOKIE")
+  def __init__(self):
+    self.pattern = r'https?://www.furaffinity.net/(?:view|full)/(?P<id>\d+)'
+    self._cookie = config("FA_COOKIE")
 
-    async def extract(self, match:Match, session: aiohttp.ClientSession) -> SauceResponse:
-        submission_id = match.group("id")
+  async def extract(self, match:Match, session: aiohttp.ClientSession) -> SauceResponse:
+    submission_id = match.group("id")
 
-        url = f'http://www.furaffinity.net/view/{submission_id}'
-        async with session.get(url, headers={'Cookie': self._cookie}) as response:
-            text = await response.read()
-            soup = BeautifulSoup(text, "html.parser")
+    url = f'http://www.furaffinity.net/view/{submission_id}'
+    async with session.get(url, headers={'Cookie': self._cookie}) as response:
+      text = await response.read()
+      soup = BeautifulSoup(text, "html.parser")
 
-            title, _, author = soup.find('meta', property='og:title')['content'].rpartition(" by ")
-            icon_url = 'https:' + soup.select('.classic-submission-title img.avatar')[0]['src']
+      title, _, author = soup.find('meta', property='og:title')['content'].rpartition(" by ")
+      icon_url = 'https:' + soup.select('.classic-submission-title img.avatar')[0]['src']
 
-            description = soup.select('.maintable .maintable tr:nth-of-type(2) td')[0].get_text()
-            description = '\n'.join([l for l in description.split('\n') if l])
-            description = (description[:197] + '...') if len(description) > 200 else description
+      description = soup.select('.maintable .maintable tr:nth-of-type(2) td')[0].get_text()
+      description = '\n'.join([l for l in description.split('\n') if l])
+      description = (description[:197] + '...') if len(description) > 200 else description
 
-            rating = soup.find('meta', attrs={'name': 'twitter:data2'})['content']
+      rating = soup.find('meta', attrs={'name': 'twitter:data2'})['content']
 
-            img = 'https:' + soup.select('#submissionImg')[0]['data-fullview-src']
+      img = 'https:' + soup.select('#submissionImg')[0]['data-fullview-src']
 
-            if rating == "General":
-                return None
-            return SauceResponse(title=title, description=description, url=url, author_name=author, author_icon=icon_url, image=img)
-            
-            #{'url': url, 'name': author, 'icon_url': icon_url, 'title': title, 'description': description, 'images': [img]}
+      if rating == "General":
+        return None
+      return SauceResponse(title=title, description=description, url=url, author_name=author, author_icon=icon_url, image=img)
+      
+      #{'url': url, 'name': author, 'icon_url': icon_url, 'title': title, 'description': description, 'images': [img]}
+
+  async def cleanup(self, match:Match) -> None:
+    pass
--- sauce/ladles/inkbunny.py
+++ sauce/ladles/inkbunny.py
@@ -1,6 +1,7 @@
 import aiohttp, json, re, scalpl
 from hikari import Color
 from decouple import config
+from re import Match
 import logging
 
 from sauce.response import SauceResponse
@@ -9,55 +10,58 @@ from . import Ladle
 logger = logging.getLogger("InkBunny")
 
 class InkBunny(Ladle):
-    def __init__(self):
-        self.pattern = r'https?://.*inkbunny\.net/s/(?P<id>\d+)'
-        #self.hotlinking_allowed = True
-        self.__sid = "" # Cannot start as None or else requests fail-unsafe
+  def __init__(self):
+    self.pattern = r'https?://.*inkbunny\.net/s/(?P<id>\d+)'
+    #self.hotlinking_allowed = True
+    self.__sid = "" # Cannot start as None or else requests fail-unsafe
 
-    async def _login(self, session: aiohttp.ClientSession) -> None:
-      user_info = {
-        "username" : config("IB_USER", cast=str),
-        "password" : config("IB_PASS", cast=str)
-      }
-      logger.info(f"Logging into InkBunny as user {user_info['username']}")
-      async with session.get(url='https://inkbunny.net/api_login.php', params = user_info, headers = {'User-Agent': 'sauce/0.1'}) as response:
-          text = await response.read()
-          data = json.loads(text)
-          try:
-            self.__sid = data["sid"]
-            logger.info(f"Obtained SID <{self.__sid}> from Inkbunny")
-          except KeyError as e:
-            logger.critical(f'Could not login to InkBunny:\nResponse from server: {data["error_message"]}')
+  async def _login(self, session: aiohttp.ClientSession) -> None:
+    user_info = {
+      "username" : config("IB_USER", cast=str),
+      "password" : config("IB_PASS", cast=str)
+    }
+    logger.info(f"Logging into InkBunny as user {user_info['username']}")
+    async with session.get(url='https://inkbunny.net/api_login.php', params = user_info, headers = {'User-Agent': 'sauce/0.1'}) as response:
+      text = await response.read()
+      data = json.loads(text)
+      try:
+        self.__sid = data["sid"]
+        logger.info(f"Obtained SID <{self.__sid}> from Inkbunny")
+      except KeyError as e:
+        logger.critical(f'Could not login to InkBunny:\nResponse from server: {data["error_message"]}')
 
-    async def extract(self, match: re.Match, session: aiohttp.ClientSession) -> SauceResponse:
-        if not self.__sid:
-          await self._login(session)
-        
-        params = {
-          "sid" : self.__sid,
-          "submission_ids" : match.group("id")
-        }
+  async def extract(self, match: re.Match, session: aiohttp.ClientSession) -> SauceResponse:
+    if not self.__sid:
+      await self._login(session)
+    
+    params = {
+      "sid" : self.__sid,
+      "submission_ids" : match.group("id")
+    }
 
-        request_url = 'https://inkbunny.net/api_submissions.php'
-        async with session.get(request_url, params=params, headers={'User-Agent': 'sauce/0.1'}) as response:
-            text = await response.read()
-            data = json.loads(text)
-            if "error_code" in data:
-              if int(data["error_code"]) == 2:
-                logger.warning("Logged out; Retrying...")
-                await self._login(session) # Refresh SID
-                await self.extract(match, session)
-            else:
-              submission = scalpl.Cut(data["submissions"][0])
+    request_url = 'https://inkbunny.net/api_submissions.php'
+    async with session.get(request_url, params=params, headers={'User-Agent': 'sauce/0.1'}) as response:
+      text = await response.read()
+      data = json.loads(text)
+      if "error_code" in data:
+        if int(data["error_code"]) == 2:
+          logger.warning("Logged out; Retrying...")
+          await self._login(session) # Refresh SID
+          await self.extract(match, session)
+      else:
+        submission = scalpl.Cut(data["submissions"][0])
 
-              response = SauceResponse(
-                title = submission.get("title"),
-                description=None,
-                url=match[0],
-                images = [f.get("file_url_full") for f in submission.get("files")],
-                author_name = submission.get("username"),
-                author_icon = submission.get("user_icon_url_small") or r'https://qc.ib.metapix.net/images78/usericons/large/noicon.png',
-                author_url = f"https://www.inkbunny.net/{submission.get('username')}",
-                color = Color(0x73d216)
-              )
-              return response
+        response = SauceResponse(
+          title = submission.get("title"),
+          description=None,
+          url=match[0],
+          images = [f.get("file_url_full") for f in submission.get("files")],
+          author_name = submission.get("username"),
+          author_icon = submission.get("user_icon_url_small") or r'https://qc.ib.metapix.net/images78/usericons/large/noicon.png',
+          author_url = f"https://www.inkbunny.net/{submission.get('username')}",
+          color = Color(0x73d216)
+        )
+        return response
+              
+  async def cleanup(self, match:Match) -> None:
+    pass
