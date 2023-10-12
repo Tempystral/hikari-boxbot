b42db210db036043c7340a570dfde07dfc9b32d7
Tempystral <elijah.doern@gmail.com>
2023-10-12 18:39:51 -0400

    Changed ESixPool ladle to sort images by pool index

--- bot/ladles/esix.py
+++ bot/ladles/esix.py
@@ -25,7 +25,7 @@ class ESixPool(Ladle):
     try:
       pool = await _api.get_pool(pool_id)
       posts = await self.__get_pool_images(pool)
-      posts = sorted(posts, key=lambda p: p.created_at)
+      posts = sorted(posts, key=lambda p: pool.post_ids.index(p.id))
     except EsixApiException as e:
       logger.warning(f"Could not retrieve e621 pool data due to error {e.code}: {e.message} -> {e.data}")
       return
@@ -46,55 +46,3 @@ class ESixPool(Ladle):
     
   async def cleanup(self, match:Match) -> None:
       pass
-
-"""class ESixPost(BaseInfoExtractor):
-  def __init__(self):
-    self.pattern = r'https?://(?P<site>e621|e926)\.net/posts/(?P<id>\d+)'
-
-  async def extract(self, url: str, session: ClientSession) -> Optional[Dict]:
-    groups = re.match(self.pattern, url).groupdict()
-    post_id = groups['id']
-
-    data = await _api.get(f'/posts/{post_id}.json', session)
-    post = data['post']
-    tags = post['tags']['general']
-    #print(data)
-    
-    # Determine if post is embed-blacklisted
-    blacklisted = any([tag in tags for tag in ['loli', 'shota']])
-    blacklisted = blacklisted or ('young' in tags and not post['rating'] == 's')
-    if blacklisted:
-      images = {'images': [post['file']['url']]}
-      if post['file']['ext'] in ['webm']:
-        return images
-      if post['file']['ext'] in ['jpg', 'png', 'gif']:
-        characters = post["tags"]["character"]
-        artists = post["tags"]["artist"]
-        title = f"{commaList(characters)} drawn by {commaList(artists)}"
-        return {'url': url, 'title': title, 'description': post["description"], 'images': [post['file']['url']]}
-
-
-class ESixDirectLink(Ladle):
-  '''Extractor to source E621 and E926 direct image links'''
-  def __init__(self):
-    self.pattern = r'https?://static1\.(?P<site>e621|e926)\.net/data/(sample/)?../../(?P<md5>\w+)\..*'
-
-  async def extract(self, match:Match, session: ClientSession) -> SauceResponse:
-    image_md5 = match.group("md5")
-    data = await _api.get('/posts.json?tags=md5%3A' + image_md5, session)
-    post_url = "https://e621.net/posts/" + str(data['posts'][0]['id'])
-    response = SauceResponse(
-      url=post_url,
-      description = data["description"],
-
-    )
-    return response"""
-      
-'''
-artists = [data["artist"][k] for k in data["artist"].keys()]
-return {'name': ", ".join([a.capitalize() for a in artists if a != "conditional_dnp"]),
-    'description': data.get('description'),
-    'images': urls,
-    'count': data['post_count']
-    }
-'''
