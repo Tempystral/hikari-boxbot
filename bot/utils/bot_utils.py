f919b52f0619cc8e683e455b411f9e56aa137fa0
Tempystral <elijah.doern@gmail.com>
2025-11-08 21:18:51 -0500

    Wow that was dumb

--- bot/utils/bot_utils.py
+++ bot/utils/bot_utils.py
@@ -9,13 +9,13 @@ import lightbulb
 async def get_emoji(bot: lightbulb.BotApp, guilds: Sequence[hikari.Snowflakeish]):
   coros = []
   for guild in guilds:
-    c = get_emoji(bot, guild)
+    c = make_coros(bot, guild)
     if c != None:
       coros.append(c)
   emojilists:Sequence[hikari.Emoji] = await asyncio.gather(*coros)
   return dict(zip(guilds, emojilists))
 
-def get_emoji(bot: lightbulb.BotApp, guild: hikari.Snowflakeish):
+def make_coros(bot: lightbulb.BotApp, guild: hikari.Snowflakeish):
   try:
     return bot.rest.fetch_guild_emojis(guild)
   except hikari.errors.NotFoundError as e:
