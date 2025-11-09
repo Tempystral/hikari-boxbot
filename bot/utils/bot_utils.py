af63f8e28e3bc56518b82a548e4dbe429ee6ca85
Tempystral <elijah.doern@gmail.com>
2025-11-08 21:09:21 -0500

    Don't error out on getting emoji

--- bot/utils/bot_utils.py
+++ bot/utils/bot_utils.py
@@ -2,10 +2,17 @@ import asyncio
 from typing import Sequence
 
 import hikari
+import hikari.errors
 import lightbulb
 
 
 async def get_emoji(bot: lightbulb.BotApp, guilds: Sequence[hikari.Snowflakeish]):
-  coros = [ bot.rest.fetch_guild_emojis(guild) for guild in guilds ]
-  emojilists:Sequence[hikari.Emoji] = await asyncio.gather(*coros)
+  coros = [ get_emoji(bot, guild) for guild in guilds ]
+  emojilists:Sequence[hikari.Emoji] = await asyncio.gather(*[c for c in coros if c != None])
   return dict(zip(guilds, emojilists))
+
+def get_emoji(bot: lightbulb.BotApp, guild: hikari.Snowflakeish):
+  try:
+    return bot.rest.fetch_guild_emojis(guild)
+  except hikari.errors.NotFoundError as e:
+    return None
