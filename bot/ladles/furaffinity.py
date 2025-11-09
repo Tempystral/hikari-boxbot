da26012e6bf538928d8cb0c0b74a760ff1145568
Tempystral <elijah.doern@gmail.com>
2025-11-08 20:59:12 -0500

    Okay a little differently this time

--- bot/ladles/furaffinity.py
+++ bot/ladles/furaffinity.py
@@ -15,44 +15,45 @@ class Furaffinity(Ladle):
     self.pattern = r'https?://www.furaffinity.net/(?:view|full)/(?P<id>\d+)'
     self._cookie = config("FA_COOKIE")
     self._headers = {'Cookie': self._cookie}
+    self._session: ClientSession | None = None
 
   async def extract(self, match:Match, session: ClientSession) -> SauceResponse:
     submission_id = match.group("id")
-
     url = f'http://www.furaffinity.net/view/{submission_id}'
-    session.headers.merge(self._headers)
-    async with session.get(url, headers=self._headers) as response:
-      text = await response.read()
-      soup = BeautifulSoup(text, "html.parser")
+    
+    async with ClientSession(headers=self._headers) as faSession:      
+      async with faSession.get(url, headers=self._headers) as response:
+        text = await response.read()
+        soup = BeautifulSoup(text, "html.parser")
 
-      rating = soup.find('meta', attrs={'name': 'twitter:data2'})['content']
-      if rating == "General":
-        return None # No need to block the thread over this
+        rating = soup.find('meta', attrs={'name': 'twitter:data2'})['content']
+        if rating == "General":
+          return None # No need to block the thread over this
 
-      title, _, author = soup.find('meta', property='og:title')['content'].rpartition(" by ")
-      icon_url = 'https:' + soup.select('.submission-user-icon')[0]['src']
+        title, _, author = soup.find('meta', property='og:title')['content'].rpartition(" by ")
+        icon_url = 'https:' + soup.select('.submission-user-icon')[0]['src']
 
-      description = soup.select('.submission-description')[0].get_text()
-      description = '\n'.join([l for l in description.split('\n') if l])
-      description = (description[:197] + '...') if len(description) > 200 else description
+        description = soup.select('.submission-description')[0].get_text()
+        description = '\n'.join([l for l in description.split('\n') if l])
+        description = (description[:197] + '...') if len(description) > 200 else description
 
-      img = 'https:' + soup.select('#submissionImg')[0]['data-fullview-src']
+        img = 'https:' + soup.select('#submissionImg')[0]['data-fullview-src']
 
-      upload_date = soup.select(".popup_date")[0]['data-time']
-      date = datetime.fromtimestamp(int(upload_date), timezone.utc)
+        upload_date = soup.select(".popup_date")[0]['data-time']
+        date = datetime.fromtimestamp(int(upload_date), timezone.utc)
 
-      return SauceResponse(
-        title=title,
-        description=description,
-        url=url,
-        author_name=author,
-        author_icon=icon_url,
-        images=[img],
-        timestamp=date,
-        color=Color(0xfaaf3a)
-        )
-      
-      #{'url': url, 'name': author, 'icon_url': icon_url, 'title': title, 'description': description, 'images': [img]}
+        return SauceResponse(
+          title=title,
+          description=description,
+          url=url,
+          author_name=author,
+          author_icon=icon_url,
+          images=[img],
+          timestamp=date,
+          color=Color(0xfaaf3a)
+          )
+        
+        #{'url': url, 'name': author, 'icon_url': icon_url, 'title': title, 'description': description, 'images': [img]}
 
   async def cleanup(self, match:Match) -> None:
     pass
